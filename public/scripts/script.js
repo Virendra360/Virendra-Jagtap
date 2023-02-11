dad4d3919e7eb1f012dec7b58feb77e1fc7e1e7d
Virendra <viru03062000@gmail.com>
2023-02-11 19:26:16 +0530

    added projects and ready to move on server

---
+++ public/scripts/script.js
@@ -0,0 +1,39 @@
+// Js code
+
+function navToGitHub(event)
+{
+    
+     switch(event.target.name) {
+        case "roadtojscode":
+              location.href ="https://github.com/Virendra360/RoadToJsDev.git";
+          break;
+        case "liveweathercode":
+            location.href ="https://github.com/Virendra360/WeatherApp.git";
+          break;
+        default:
+                 alert("Something went wrong!!")
+      }
+
+}
+
+function openProject(event)
+{
+    
+    switch(event.target.name) {
+        case "roadtojsdemo":
+              location.href ="https://road-to-js-dev-virendra360.vercel.app/";
+          break;
+        case "liveweatherdemo":
+            location.href ="https://weather-app-versel-virendra360.vercel.app/";
+          break;
+        default:
+                 alert("Something went wrong!!")
+      }
+
+}
+
+// navigate to home from error page
+// function navToHome()
+// {
+//   location.href ="/";
+// }
