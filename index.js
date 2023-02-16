68a71f044885849b51cd8136fbcc7c4e72d2e2a7
Virendra <viru03062000@gmail.com>
2023-02-16 21:25:47 +0530

    Added React Skill and initialise template engine

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var express = require("express");
 var path= require("path");
+var hbs= require("hbs");  //view engine of handlebars
 
 var PORT= 7000;
-
 var app= express();
 
 // Use of static path
 var static_path= path.join(__dirname); //declaring folder in which index.html is present
 app.use(express.static(static_path));
 
+// use of template engine handlebars 
+var template_path= path.join(__dirname);
 
+
+app.get('*',(req,res)=>{
+    res.send("404 Error");
+})
 
 app.listen(PORT,()=>{
     console.log(`Server started: http://localhost:${PORT}`);
